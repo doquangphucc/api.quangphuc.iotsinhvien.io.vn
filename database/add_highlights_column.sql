7544d2769f572206e0549485bf5450552786ab9e
doquangphucc <phucdq.21ce@vku.udn.vn>
2025-10-27 05:05:02 +0700

    Simplify add highlights column SQL

--- database/add_highlights_column.sql
+++ database/add_highlights_column.sql
@@ -1,6 +1,5 @@
 -- Add highlights column to packages table
--- Run this SQL file on your existing database
 
 ALTER TABLE packages 
-ADD COLUMN IF NOT EXISTS highlights TEXT COMMENT 'JSON array of highlights: [{"title":"...", "content":"..."}]' 
+ADD COLUMN highlights TEXT COMMENT 'JSON array of highlights: [{"title":"...", "content":"..."}]' 
 AFTER payback_period;
