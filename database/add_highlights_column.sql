d5eacc4fcf6d030b74494ac493e4110b75ab76dd
doquangphucc <phucdq.21ce@vku.udn.vn>
2025-10-27 05:06:53 +0700

    Delete add_highlights_column.sql - no longer needed

--- database/add_highlights_column.sql
+++
@@ -1,5 +0,0 @@
--- Add highlights column to packages table
-
-ALTER TABLE packages 
-ADD COLUMN highlights TEXT COMMENT 'JSON array of highlights: [{"title":"...", "content":"..."}]' 
-AFTER payback_period;
